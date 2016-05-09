3061d3e20834002a4ff8bb0c12af388deae8bed0
ycd006 <ycd006@RUN.local>
2016-05-09 14:19:02 +0800

    12

---
+++ DrawerView/CenterViewController.h
@@ -0,0 +1,13 @@
+//
+//  CenterViewController.h
+//  DrawerView
+//
+//  Created by ycd006 on 16/5/9.
+//  Copyright © 2016年 闰尚松. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CenterViewController : UIViewController
+
+@end
---
+++ DrawerView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DrawerView
+//
+//  Created by ycd006 on 16/5/9.
+//  Copyright © 2016年 闰尚松. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
